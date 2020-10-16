95a285e64592d9d8c99901a5fb4b50596689fbc8
ViczianGabor <viczian.gabor13@gmail.com>
2020-10-16 12:56:08 +0200

    Add project files.

---
+++ balkezesek/Balosok.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balkezesek
+{
+    class Balosok
+    {
+        public string Nev { get;private set; }
+
+        public string ElsoDatum { get;private set; }
+
+        public string UtolsoDatum { get; private set; }
+
+        public int Suly { get; private set; }
+
+        public int Magassag { get; private set; }
+
+
+
+        public Balosok(string szoveg)
+        {
+            int i = 0;
+            string[] a = szoveg.Split(';');
+            Nev = a[0];
+            ElsoDatum = a[1];
+            UtolsoDatum = a[2];
+            Suly = int.Parse(a[3]);
+            Magassag = int.Parse(a[4]);
+            i++;
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ balkezesek/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace balkezesek
+{
+    class Program
+    {
+        static List<Balosok> lista = new List<Balosok>();
+        static int szam = 0;
+        public List<string> hatos = new List<string>();
+
+        static void Beolvas()
+        {
+            StreamReader sr = new StreamReader("balkezesek.csv");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                lista.Add(new Balosok(sr.ReadLine()));
+
+
+
+            }
+
+
+            sr.Close();
+
+
+            
+
+        }
+
+
+        static void harmadik()
+        {
+            Console.Write("3. feladat: ");
+            Console.WriteLine(lista.Count);
+
+
+        }
+
+        static void negyedik()
+        {
+            Console.Write("4. feladat: \n");
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (lista[i].UtolsoDatum.Contains("1999-10"))
+                {
+                    Console.WriteLine($"\t{lista[i].Nev} {lista[i].Magassag*2.54:N1} cm");
+                }
+            }
+        }
+
+        static void otodik()
+        {
+            Console.WriteLine("5. feladat");
+            Console.Write("Kérek egy 1990 és 1999 közötti évszámot!: ");
+            szam = int.Parse(Console.ReadLine());
+            bool helyes = false;
+            while (!helyes)
+            {
+                if (szam <1990 || szam > 1999)
+                {
+                    Console.Write("Hibása adat! Kérek egy 1990 és 1999 közötti évszámot!: ");
+                    szam = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    helyes = true;
+                }
+            }
+            
+
+
+
+
+        }
+
+        static void hatodik()
+        {
+            Console.Write("6. feladat:");
+            double osszeg = 0;
+            double db = 0;
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (szam)
+                {
+
+                }
+            }
+            
+
+            Console.WriteLine($"{atlag:N2}");
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Beolvas();
+            harmadik();
+            negyedik();
+            otodik();
+         
+            hatodik();
+
+
+            //Jim Abbott;1989-04-08;1999-07-21;200;75
+
+            Console.ReadKey();
+        }
+    }
+}
